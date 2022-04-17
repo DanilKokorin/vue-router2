b9bdc72db54f7acad45ac8b822804159af07fe9e
Danil Kokorin <63406852+DanilKokorin@users.noreply.github.com>
2022-04-17 22:23:37 +0300

    Add files via upload

---
+++ src/router.js
@@ -0,0 +1,63 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from './views/Login'
+import Forget from './views/Forget'
+import Dashboard from './views/Dashboard'
+// import Mail from './views/Mail'
+import AppEmailBody from './components/AppEmailBody'
+import NotFound from './views/NotFound'
+
+const Mail = () => import('./views/Mail')
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { 
+            path: '/login',
+            component: Login,
+            alias: '/'
+        },
+        { 
+            path: '/forget',
+            component: Forget,
+            meta: {cantEnter: true}
+        },
+        { 
+            path: '/dashboard',
+            component: Dashboard,
+            name: 'home',
+            beforeEnter () {
+                console.log('beforeEnter');
+            }
+        },
+        { 
+            name:'email',
+            path: '/mail',
+            component: Mail,
+            children: [
+                { path: ':mailId', component: AppEmailBody, props: true }
+            ]
+        },
+        {
+            path: '/:notFound(.*)',
+            component: NotFound
+        }
+    ],
+    linkActiveClass: 'active',
+    linkExactActiveClass: 'active'
+})
+
+router.beforeEach( (to, from, next) => {
+    if (to.meta.cantEnter) {
+        next({
+            name: 'home'
+        })
+    } else {
+        next()
+    }
+})
+
+router.afterEach( (to, from) => {
+
+})
+
+export default router
---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="card">
+        <h1>404</h1>
+        <p>Страница не найдена</p>
+        <router-link to="/login">Авторизоваться</router-link>
+    </div>
+</template>
+
+<style scoped>
+    .card {
+        text-align: center;
+    }
+</style>
